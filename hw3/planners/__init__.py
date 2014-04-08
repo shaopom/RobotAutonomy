0f1f08b01c3d0c7c9d084d7a97cbdceb83be27a1
Unchapped <nate@nathanielchapman.com>
2014-04-08 17:57:19 -0400

    fixed tab conversion issue

--- hw3/planners/__init__.py
+++ hw3/planners/__init__.py
@@ -1,6 +1,5 @@
 import time
 class Planner(object):
-    
     def __init__(self, planning_env, visualize):
         self.planning_env = planning_env
         self.visualize = visualize and hasattr(self.planning_env, 'InitializePlot')
@@ -8,21 +7,21 @@ class Planner(object):
         self.node_count = 0
         self.plan_time = 0
         
-	def Plan(self, start_config, goal_config):
-		"""planner wrapper function to abstract statistics away from individual planners"""
-		if self.visualize:
-			self.planning_env.InitializePlot(goal_config)
+    def Plan(self, start_config, goal_config):
+        """planner wrapper function to abstract statistics away from individual planners"""
+        if self.visualize:
+            self.planning_env.InitializePlot(goal_config)
 
-		start_time = time.clock()
-		plan = self.DoPlan(start_config, goal_config)
-		self.plan_time = time.clock() - start_time
+        start_time = time.clock()
+        plan = self.DoPlan(start_config, goal_config)
+        self.plan_time = time.clock() - start_time
 
-		if plan: #non-empty path
-			for i in range(len(plan)-1):
-				self.path_length += self.planning_env.ComputeDistanceConfig(plan[i], plan[i+1])
-			return plan
-		else:
-			return None #obvious planning failure
+        if plan: #non-empty path
+            for i in range(len(plan)-1):
+                self.path_length += self.planning_env.ComputeDistanceConfig(plan[i], plan[i+1])
+            return plan
+        else:
+            return None #obvious planning failure
 
     def DoPlan(self, start_config, goal_config):
         return None
